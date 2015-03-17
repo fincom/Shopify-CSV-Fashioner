24b74990f7b1ec27ea9a9a2f30ea01216fdfdd86
Eric Patrick <erstpa@gmail.com>
2015-03-17 13:55:45 -0400

    Added an array of product types

--- main.php
+++ main.php
@@ -7,8 +7,18 @@ include 'functions.php';
 //  regular, lightduty, wide, fob, neckid, necklace, neckcord
 include 'variables.php';
 
+$product_types = array('regular', 'lightduty', 'wide', 'fob', 'neckid', 'necklace', 'neckcord');
+
 $new_product_name = "SWAT";
 $new_product_name_friendly = seoUrl($new_product_name);
+
+$type_title_full = "Regular Survival Bracelet™ - $new_product_name";
+$type_title_full = "Light Duty Survival Bracelet™ - $new_product_name";
+$type_title_full = "Wide Survival Bracelet™ - $new_product_name";
+$type_title_full = "Key Fob - $new_product_name";
+$type_title_full = "Gear Tag - $new_product_name";
+$type_title_full = "Neck ID Lanyard - $new_product_name";
+
 
 
 $url = "";
