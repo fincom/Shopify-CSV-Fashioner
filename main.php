8ee3c7abf84df74995f6f568b2d7ec34a50e77c9
Eric Patrick <erstpa@gmail.com>
2015-03-17 13:47:39 -0400

    Included functions.php

--- main.php
+++ main.php
@@ -1,8 +1,19 @@
 <?php
 
+// seoUrl
+include 'functions.php';
+
+// Include HTML for each product type: 
+//  regular, lightduty, wide, fob, neckid, necklace, neckcord
+include 'variables.php';
+
+$new_product_name = "SWAT";
+$new_product_name_friendly = seoUrl($new_product_name);
+
 
 $url = "";
 $title = "";
+$html = "";
 $vendor = "Pre-decal";
 $type = "";
 $tags = "";
@@ -13,9 +24,7 @@ $imgalt = "";
 
 
 echo <<<_OUTPUT
-
-$url,$title,"HTML",$vendor,$type,$tags,FALSE,Title,Default Title,,,,,$sku,0,,1,deny,manual,$price,,TRUE,TRUE,,$img,$imgalt,FALSE,,,,,,,,,,,,,,,,,lb
-
+$url,$title,"$html",$vendor,$type,$tags,FALSE,Title,Default Title,,,,,$sku,0,,1,deny,manual,$price,,TRUE,TRUE,,$img,$imgalt,FALSE,,,,,,,,,,,,,,,,,lb
 _OUTPUT;
 
 ?>
