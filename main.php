9785dcdd368fd9debbba3ca40cebf8b9b5c51973
Eric Patrick <erstpa@gmail.com>
2015-03-17 12:26:49 -0400

    Created base output and variables

--- main.php
+++ main.php
@@ -1,2 +1,21 @@
-<?
+<?php
 
+
+$url = "";
+$title = "";
+$vendor = "Pre-decal";
+$type = "";
+$tags = "";
+$sku = "";
+$price = "";
+$img = "";
+$imgalt = "";
+
+
+echo <<<_OUTPUT
+
+$url,$title,"HTML",$vendor,$type,$tags,FALSE,Title,Default Title,,,,,$sku,0,,1,deny,manual,$price,,TRUE,TRUE,,$img,$imgalt,FALSE,,,,,,,,,,,,,,,,,lb
+
+_OUTPUT;
+
+?>
