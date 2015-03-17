071f2e747268575648da459b67fcbd00462821f9
Eric Patrick <erstpa@gmail.com>
2015-03-17 14:45:33 -0400

    Filled in the variables

--- main.php
+++ main.php
@@ -7,34 +7,46 @@ include 'functions.php';
 //  regular, lightduty, wide, fob, neckid, necklace, neckcord
 include 'variables.php';
 
-$product_types = array('regular', 'lightduty', 'wide', 'fob', 'neckid', 'necklace', 'neckcord');
 
 $new_product_name = "SWAT";
+$new_product_type = "Law Enforcement";
+$new_product_production = "Pre-decal";
+$sku = "201110000003180";
+
+
 $new_product_name_friendly = seoUrl($new_product_name);
 
-$type_title_full = "Regular Survival Bracelet™ - $new_product_name";
-$type_title_full = "Light Duty Survival Bracelet™ - $new_product_name";
-$type_title_full = "Wide Survival Bracelet™ - $new_product_name";
-$type_title_full = "Key Fob - $new_product_name";
-$type_title_full = "Gear Tag - $new_product_name";
-$type_title_full = "Neck ID Lanyard - $new_product_name";
+$product_type_title = array("Regular Survival Bracelet™ - $new_product_name", "Light Duty Survival Bracelet™ - $new_product_name", "Wide Survival Bracelet™ - $new_product_name", "Key Fob - $new_product_name", "Gear Tag - $new_product_name", "Neck ID Lanyard - $new_product_name", "Necklace - $new_product_name", "Neck Cord - $new_product_name");
+$product_types = array('regular', 'lightduty', 'wide', 'fob', 'geartag', 'neckid', 'necklace', 'neckcord');
+$product_prices = array('24.99', '24.99', '26.99', '19.95', '12.95','21.95','29.99','14.95');
 
+$count = 0;
 
+foreach($product_type_title as $title) {
 
-$url = "";
-$title = "";
-$html = "";
-$vendor = "Pre-decal";
-$type = "";
-$tags = "";
-$sku = "";
-$price = "";
-$img = "";
-$imgalt = "";
+		$url = seoUrl($title);
+		$html = "";
+		$vendor = $new_product_production;
+		$type = $new_product_type;
+		$tags = $product_types[$count];
+		$price = $product_prices[$count];
+		$img = "https://sstrapfiles.s3.amazonaws.com/shopify/product_photos/" . $new_product_name_friendly . "/" . $product_types[$count] . ".jpg";
+		$imgalt = $title;
 
 
 echo <<<_OUTPUT
-$url,$title,"$html",$vendor,$type,$tags,FALSE,Title,Default Title,,,,,$sku,0,,1,deny,manual,$price,,TRUE,TRUE,,$img,$imgalt,FALSE,,,,,,,,,,,,,,,,,lb
+$url,$title,"$html",$vendor,$type,$tags,FALSE,Title,Default Title,,,,,$sku,0,,1,deny,manual,$price,,TRUE,TRUE,,$img,$imgalt,FALSE,,,,,,,,,,,,,,,,,lb\n
 _OUTPUT;
+
+		++$sku;
+		$sku = number_format($sku,0,'','');
+		++$count;
+
+	}
+
+--$sku;
+$sku = number_format($sku,0,'','');
+echo "<br><br>$count total products added.<br><br>$sku";
+
 
 ?>
