dac6fa8ad31bc83c713d08b3a5547636aed613d3
Eric Patrick <erstpa@gmail.com>
2015-03-27 14:23:33 -0400

    Added lines for CSV headers so you can just save the webpage as .csv now

--- main.php
+++ main.php
@@ -10,12 +10,12 @@ include 'functions.php';
 //  regular, lightduty, wide, fob, neckid, necklace, neckcord
 include 'variables.php';
 
-$new_products = array('USMC', 'White Semper Fi', 'Red Semper Fi', 'Gold Semper Fi', 'USMC Semper Fidelis', 'Silver Semper Fi', 'Gold USMC Logo', 'Gold USMC', 'Red USMC', 'USMC Semper Fi');
-$new_product_colors = array('black', 'multicam', 'redgold');
-$new_product_type = "USMC";
+$new_products = array('Licensed Nursing Assistant');
+$new_product_colors = array('black');
+$new_product_type = "LNA";
 $new_product_type_friendly = seoUrl($new_product_type);
 $new_product_production = "Pre-sticker";
-$sku = "201110000003240";
+$sku = "201110000003320";
 
 foreach($new_products as $new_product_name) 
 	{
@@ -59,7 +59,9 @@ foreach($new_products as $new_product_name)
 
 			break;
 			
-			default:
+			case 'csv':
+				//Output CSV headers
+				echo "Handle,Title,Body (HTML),Vendor,Type,Tags,Published,Option1 Name,Option1 Value,Option2 Name,Option2 Value,Option3 Name,Option3 Value,Variant SKU,Variant Grams,Variant Inventory Tracker,Variant Inventory Qty,Variant Inventory Policy,Variant Fulfillment Service,Variant Price,Variant Compare At Price,Variant Requires Shipping,Variant Taxable,Variant Barcode,Image Src,Image Alt Text,Gift Card,Google Shopping / MPN,Google Shopping / Age Group,Google Shopping / Gender,Google Shopping / Google Product Category,SEO Title,SEO Description,Google Shopping / AdWords Grouping,Google Shopping / AdWords Labels,Google Shopping / Condition,Google Shopping / Custom Product,Google Shopping / Custom Label 0,Google Shopping / Custom Label 1,Google Shopping / Custom Label 2,Google Shopping / Custom Label 3,Google Shopping / Custom Label 4,Variant Image,Variant Weight Unit\n";
 
 				foreach($product_type_title as $title) 
 					{
@@ -69,6 +71,7 @@ foreach($new_products as $new_product_name)
 						$type = $new_product_type;
 						$tags = $product_types[$count];
 						$price = $product_prices[$count];
+
 						//Output each line in CSV format
 						echo "$url,$title,\"$html\",$vendor,$type,$tags,FALSE,Title,Default Title,,,,,$sku,0,,1,deny,manual,$price,,TRUE,TRUE,,,,FALSE,,,,,,,,,,,,,,,,,lb\n";
 						// Randomize the colors because "mix them up" is the initiative
